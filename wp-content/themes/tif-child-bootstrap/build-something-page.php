32ba97742351e891a8c4ad3b912eac51082712aa
jtrueblood <jamie@thinkitfirst.com>
2020-09-16 10:14:01 -0400

    reset build something page

--- wp-content/themes/tif-child-bootstrap/build-something-page.php
+++ wp-content/themes/tif-child-bootstrap/build-something-page.php
@@ -31,104 +31,15 @@ $playerassoc = get_players_assoc();
 				<!--Page content-->
 				<div id="page-content">
 					
-					
-						
-						<div class="panel panel-bordered panel-light">
-							<div class="panel-heading">
-								<h3 class="panel-title">Create New Player</h3>
-							</div>
-								<div class="panel-body">
-									<div class="col-xs-24">
-										<p><small>Check for player above and if they do not exist enter MFL ID below.</small></p>	
-										<form action="" method="post">
-
-											MFL ID: <input type="text" name="mflid" /><br>
-											<br>
-											<input type="submit" />
-											
-											
-										</form>
-										<p><small>Year and possibly league ID value needs to be updated for function get_mfl_player_details()</small></p>
-									</div>
-									
-									<div class="col-xs-24">
-										
-										
-										<?php 
-											if ( isset( $_POST['mflid'] ) ){
-												
-												$form_mfl_id = 	$_POST['mflid'];
-												//echo $form_mfl_id;
-												$mfl_data = get_mfl_player_details($form_mfl_id);
-												printr($mfl_data, 0);
-												
-												$name = $mfl_data['playerProfile']['name'];
-												$xname = explode(',', $name);
-												$xxname = explode(' ', $xname[1]);
-												
-												$draftyear = date('Y');
-												
-												$first = $xxname[1];
-												$last = $xname[0];
-												$position = $xxname[3];
-												$justfour = substr($last, 0, 4);
-												
-												$themflid = $mfl_data['playerProfile']['player']['id']; 
-												$weight = $mfl_data['playerProfile']['player']['weight'];
-												$dob = $mfl_data['playerProfile']['player']['dob']; 
-												$college = $mfl_data['college'];
-												$pflid = $draftyear.$justfour.$position;
-												$height = $mfl_data['playerProfile']['player']['height'];
-																								
-												$insertplayer = array(
-													'p_id' => $pflid,
-													'playerFirst' => ltrim($first),
-													'playerLast' => $last,
-													'position' => $position,
-													'rookie' => $draftyear,
-													'mflid' => $themflid,	
-													'height' => $height,
-													'weight' => $weight,
-													'college' => $for_college,
-													'birthdate' => $dob,
-													'number' => '',
-													'pfruri' => '',
-													'pfrcurl' => '',
-													'nickname' => ''
-												);
-												
-												//var_dump($covheight);
-												//printr($explode, 0);
-	
-
-												if(isset($insertplayer)){
-													createnewplayer($insertplayer);
-													printr($insertplayer, 0);
-												}
-											
-
-										     } ?>
-										     
-									</div>
-									
-								
-								</div>
-							
+					<div class="panel panel-bordered panel-light">
+						<div class="panel-heading">
+							<h3 class="panel-title">Title</h3>
 						</div>
-						<?php 
-							echo '<h3><a href="'.$pfrlink.'" target="_blank">Pro Football Reference Link</a></h3>';
-							printr($insertplayer, 0); ?>
-<!--
-						<?php
-						//$mflid = '14803';
-						$testplayer = get_mfl_player_details(14803);
-						printr($testplayer, 0);						
-						?>
--->
-
-						
-			
-											
+						<div class="panel-body">										     
+						</div>
+								
+					</div>
+																	
 				</div><!--End page content-->
 
 			</div><!--END CONTENT CONTAINER-->

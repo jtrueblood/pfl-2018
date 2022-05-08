0dc09870bef863b073b75d8c3a0e1b800e601fed
jtrueblood <jamie@thinkitfirst.com>
2022-05-08 14:41:21 -0400

    catchup with a bunch of assets

--- wp-content/themes/tif-child-bootstrap/footer.php
+++ wp-content/themes/tif-child-bootstrap/footer.php
@@ -87,10 +87,19 @@
     
     <!-- HIGH CHARTS CDN -->
     <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
 	<script src="https://code.highcharts.com/modules/export-data.js"></script>
 	<script src="https://code.highcharts.com/modules/sankey.js"></script>
 	<script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+<!--    <script src="https://code.highcharts.com/modules/streamgraph.js"></script>-->
+    <script src="https://code.highcharts.com/modules/dumbbell.js"></script>
+    <script src="https://code.highcharts.com/modules/series-label.js"></script>
+    <script src="https://code.highcharts.com/modules/annotations.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
 
 <?php wp_footer(); ?>    
   

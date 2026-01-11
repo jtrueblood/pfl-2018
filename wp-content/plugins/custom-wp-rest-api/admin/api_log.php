3c1928ab406ce81ddd3ae241660ebab94c29c6b7
jtrueblood <jamie@thinkitfirst.com>
2026-01-11 14:26:40 -0500

    Remove plugins from git

--- wp-content/plugins/custom-wp-rest-api/admin/api_log.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-include_once('header.php');
- ?>
-  <div class="gsr_back_body">
-<div class="wraparea">
-<?php  _api_log_render(); ?>
-</div>
-</div>
--- wp-content/plugins/custom-wp-rest-api/includes/demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-$array = array('base1','base2');
-  foreach ($array as $key => $value) {
-      echo 'function '.$value.'1(){ return "ff"; }';
-
-      echo 'function '.$value.'1_permission1(){ return true;  }';
-      
-    } ?>
